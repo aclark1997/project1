8e51a0d33b3038c1d0b566d6f402951493c0f009
alex <aclark7@github.com>
2024-02-08 09:38:42 -0700

    added dictionary of decks, deckid

--- src/server.py
+++ src/server.py
@@ -11,6 +11,7 @@ RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "0", "J", "Q", "K", "A"]
 
 app = FastAPI()
 
+decks = {}
 
 @app.get("/api/v1/hello")
 async def api_v1():
@@ -25,11 +26,17 @@ async def api_v1_deal():
 @app.post("/api/v2/deck/new")
 async def api_v2_deck_new():
     d = Deck()
+    print("deck hash: " + str(hash(d)))
+    decks[str(hash(d))] = d
     return {"message": "deck created!"}
 
 
 @app.get("/api/v2/deck/{deck_id}")
 async def api_v2_deck(deck_id: str):
+    if deck_id in decks:
+        #print(decks[deck_id])
+        return decks[deck_id]
+
     print(f"need to fetch Deck {deck_id}")
     raise HTTPException(status_code=404, detail=f"Deck {deck_id} not found")
 
